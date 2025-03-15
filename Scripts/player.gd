5e102bda851b2f7c31a2b99a836dc52bdf193184
aidan <aidanhwang08@gmail.com>
2025-03-15 14:15:21 -0400

    added charater

---
+++ Scripts/player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle athe movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if Input.is_action_pressed("crouch") and is_on_floor():
+		var shape = $CollisionShape2D.shape
+		var size = shape.size
+		shape.set_size(Vector2(size.x, size.y / 2))
+		
+
+	move_and_slide()
