3162c6b4834bbfaa641ba4fc00c8244f47a82e5f
aidan <aidanhwang08@gmail.com>
2025-03-15 16:50:47 -0400

    added hitboxes

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
-
+@onready var animation_player := $AnimationPlayer
+@onready var animated_sprite := $AnimatedSprite2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-
+func take_damage(amount: int) -> void:
+	print("Damage: ", amount)
+	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -25,6 +28,10 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("crouch") and is_on_floor():
 		var shape = $CollisionShape2D.shape
 		shape.set_size(Vector2(shape.size.x, shape.size.y / 2))
+	
+	if Input.is_action_pressed("attack_high"):
+		animated_sprite.play("shaun_attacking_high")
+		animation_player.play("attack_high")
 		
 
 	move_and_slide()
