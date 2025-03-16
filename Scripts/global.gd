bd3f8045b1d4f12c06334c0ffbe2291bfba53599
aidan <aidanhwang08@gmail.com>
2025-03-16 13:42:20 -0400

    chars

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,19 +1,31 @@
 extends Node
 
-var selected_player = ""  # Variable to store data
-var health = 100
-var attack
-var heal
+var selected_player = "Shaun"  # Variable to store data
+var health
+var attack = [0, 0]
+var animation = []
 
 func _ready() -> void:
 	if selected_player == "Shaun":
-		attack = 15
-		heal = 15
+		animation.append("Shaun_attack_high")
+		animation.append("Shaun_attack_low")
+		animation.append("Shuan_crouch")
+		attack[0] = 10
+		attack.append(5)
+		health = 100
 
 	if selected_player == "Aidan":
-		attack = 25
-		heal = 5
+		animation.append("Aidan_attack_high")
+		animation.append("Aidan_attack_low")
+		animation.append("Aidan_crouch")
+		attack.append(10)
+		attack.append(15)
+		health = 100
 
 	if selected_player == "Wendy":
-		attack = 10
-		heal = 15
+		animation.append("Wendy_attack_high")
+		animation.append("Wendy_attack_low")
+		animation.append("Wendy_crouch")
+		attack.append(10)
+		attack.append(10)
+		health = 100
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -9,6 +9,7 @@ var health = 100
 var height
 var pos
 signal health_changed
+var animation = Global.animation
 
 func _enter_tree() -> void:
 	set_multiplayer_authority(int(str(name)))
@@ -23,19 +24,30 @@ func take_damage(amount: int) -> void:
 	self.health_changed.emit()
 	
 func high_attack():
+	$Hitbox.damage = Global.attack[0] 
 	if Input.is_action_pressed("attack_high"):
-		animated_sprite.play("shaun_attacking_high")
-		animation_player.play("attack_high")
+		play_animation(animation[0])
+		
+func low_attack():
+	$Hitbox.damage = Global.attack[1]
+	if Input.is_action_pressed("attack_low"):
+		play_animation(animation[1])
 	
 func crouch():
 	var crouch = height
 	var crouch_pos = pos.y
 	if Input.is_action_pressed("crouch") and is_on_floor() and !animation_player.is_playing():
+		play_animation(animation[2])
+		animation
 		crouch /= 2
 		crouch_pos = pos.y + (height / 4)
 	$CollisionShape2D.shape.height = crouch
 	$CollisionShape2D.position.y = crouch_pos
 	$Hurtbox/Hurt.position.y = crouch_pos
+	
+func play_animation(animation: String):
+	animated_sprite.play(animation)
+	animation_player.play(animation)
 	
 func _physics_process(delta: float) -> void:
 	if !is_multiplayer_authority():
@@ -53,9 +65,14 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if direction < 0:
+			self.scale = Vector2(-1, 1)
+		if direction > 0:
+			self.scale =Vector2(1, 1)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
+	
+	low_attack()
 	crouch()
 	high_attack()
 	move_and_slide()
