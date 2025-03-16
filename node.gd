298a347204dbc988db325636acb3d05a917a9467
wendyyij <jyi@columbusschoolforgirls.org>
2025-03-16 10:51:04 -0400

    done

--- node.gd
+++ node.gd
@@ -1,1 +1,25 @@
 extends Node
+
+const PLAYER = preload("res://Scenes/player.tscn")
+
+var peer=ENetMultiplayerPeer.new()
+
+func on_host_pressed():
+	peer.create_server(25565)
+	multiplayer.multiplayer_peer=peer
+	
+	multiplayer.peer_connected.connect(
+		func(pid):
+			print("Peer" + str(pid) + "Has Joined The Game!")
+			add_player()
+	)
+	
+	add_player()
+
+func on_join_pressed():
+	peer.create_client("localhost",25565)
+	multiplayer.multiplayer_peer=peer
+
+func add_player():
+	var player = PLAYER.instantiate()
+	add_child(player)
