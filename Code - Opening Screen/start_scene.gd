06af1b47f14dc78e22b0ec044adb42339c4fa7b0
Shaun Thomas <shaunbijoythomas@gmail.com>
2025-03-16 13:56:59 -0400

    final changes

--- Code - Opening Screen/start_scene.gd
+++ Code - Opening Screen/start_scene.gd
@@ -5,14 +5,14 @@ extends Control
 func _on_shaun_pressed() -> void:
 	Global.selected_player = "Shaun"  # Store selected name
 	
-	get_tree().change_scene_to_file("res://main.tscn")  # Transition to Loading Screen
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")  # Transition to Loading Screen
 	 
 func _on_aidan_pressed() -> void:
 	Global.selected_player = "Aidan"  # Store selected name
 	
-	get_tree().change_scene_to_file("res://main.tscn")  # Transition to Loading Screen
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")  # Transition to Loading Screen
 	 
 func _on_wendy_pressed() -> void:
 	Global.selected_player = "Wendy"  # Store selected name
 	
-	get_tree().change_scene_to_file("res://main.tscn")  # Transition to Loading Screen
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")  # Transition to Loading Screen
