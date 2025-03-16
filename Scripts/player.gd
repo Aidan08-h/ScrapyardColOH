2f8f181962e48effefef46a7adfecfad4c388bff
aidan <aidanhwang08@gmail.com>
2025-03-15 21:46:35 -0400

    health can decrease

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,16 +2,42 @@ extends CharacterBody2D
 
 @onready var animation_player := $AnimationPlayer
 @onready var animated_sprite := $AnimatedSprite2D
-const SPEED = 300.0
+const SPEED = 600.0
 const JUMP_VELOCITY = -400.0
+const gravity =Vector2(0, 1000.0)
+var health = 100
+var height
+var pos
+signal health_changed
+
+func _ready() -> void:
+	height = $CollisionShape2D.shape.height
+	pos = $CollisionShape2D.position
 
 func take_damage(amount: int) -> void:
-	print("Damage: ", amount)
+	health -= amount
+	print("damage: ", amount)
+	self.health_changed.emit()
+	
+func high_attack():
+	if Input.is_action_pressed("attack_high"):
+		animated_sprite.play("shaun_attacking_high")
+		animation_player.play("attack_high")
+	
+func crouch():
+	var crouch = height
+	var crouch_pos = pos.y
+	if Input.is_action_pressed("crouch") and is_on_floor() and !animation_player.is_playing():
+		crouch /= 2
+		crouch_pos = pos.y + (height / 4)
+	$CollisionShape2D.shape.height = crouch
+	$CollisionShape2D.position.y = crouch_pos
+	$Hurtbox/Hurt.position.y = crouch_pos
 	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -25,13 +51,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-	if Input.is_action_pressed("crouch") and is_on_floor():
-		var shape = $CollisionShape2D.shape
-		shape.set_size(Vector2(shape.size.x, shape.size.y / 2))
-	
-	if Input.is_action_pressed("attack_high"):
-		animated_sprite.play("shaun_attacking_high")
-		animation_player.play("attack_high")
-		
-
+	crouch()
+	high_attack()
 	move_and_slide()
